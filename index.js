46d1cc8e1b7c168baec5d3c0954344ac0940eeff
Max Falkenstein <mrfalken@iu.edu>
2018-05-14 13:30:04 -0400

    Added CSS, JS

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('It works!')
+//homework: Make the button change the text of the heading. 
+
+//bonus credit: add multiple headings to the page, and make the button
+//change the _second_ ones
+
+//super mega bonus credit: add a form to the page, add a text input to the form, update the 
+//heading with the text that you type in the text input
