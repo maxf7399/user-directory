dea6463d21e042ba738037d442007ee1602cbce1
Max Falkenstein <mrfalken@iu.edu>
2018-05-15 10:58:02 -0400

    add age

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const handleSubmit = function(ev) {
     ev.preventDefault()
     const form = ev.target
     const userName = form.userName.value
+    const age = form.age.value
     const users = document.querySelector('#users')
-    users.innerHTML += '<p>' + userName + '</p>'
+    users.innerHTML += '<p>' + userName + ', ' + age + '</p>'
     form.userName.value = ''
+    form.reset
 }
 
 form.addEventListener('submit', handleSubmit)
