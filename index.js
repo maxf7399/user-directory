55580cfb17222341a6251179d4d962f3ee813d17
Max Falkenstein <mrfalken@iu.edu>
2018-05-16 11:19:32 -0400

    show fixed code to what Davey has from class

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
 const form = document.querySelector('form#userForm')
-const handleSubmit = function(ev) {
-    ev.preventDefault()
-    const form = ev.target
-    users.appendChild(renderList())
-    form.reset()
-    form.userName.focus()
-}
-function renderColor() {
-    const favoriteColor = form.favoriteColor.value
-    const colorDiv = document.createElement('div')
-    colorDiv.style.backgroundColor = favoriteColor
-    colorDiv.style.width = '6rem'
-    colorDiv.style.height = '3rem'
-    return colorDiv
-}
-function renderListItem() { 
-    return document.createElement('li')
-}
-function renderList() { 
-    const user = {
-        'Name': form.userName.value,
-        'Age': form.age.value,
-        'Favorite Color': form.favoriteColor.value,
-    }
-   
-    /*Object.keys(user).map(function(label) { 
-        const item = renderListItem(label, user[label])
-        list.appendChild(item) this makes the .textContent and the appendChild not needed anymore
-    })   this is for renderListItem, see Davey's code for fixing of renderListItem */
-    const list = document.createElement('ul')
-    const nameItem = renderListItem()
-    nameItem.textContent = `Name: ${userName}`
-    const ageItem = renderListItem()
-    ageItem.textContent = `Age: ${age}`
-    const colorItem = renderListItem()
-    colorItem.textContent = `Favorite Color:` 
-    colorItem.appendChild(renderColor())
-    list.appendChild(nameItem)
-    list.appendChild(ageItem)
-    list.appendChild(colorItem)
 
-    return list
+function renderColor(color) {
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = color
+  colorDiv.style.width = '6rem'
+  colorDiv.style.height = '3rem'
+
+  return colorDiv
 }
+
+function renderListItem(label, value) {
+  const item = document.createElement('li')
+  item.textContent = `${label}: `
+  try {
+    item.appendChild(value)
+  } catch(e) {
+    item.textContent += value
+  }
+  return item
+}
+
+function renderList(data) {
+  const list = document.createElement('ul')
+  Object.keys(data).forEach(label => {
+    const item = renderListItem(label, data[label])
+    list.appendChild(item)
+  })
+  return list
+}
+
+const handleSubmit = function(ev) {
+  ev.preventDefault()
+  const form = ev.target
+  const user = {
+    'Name': form.userName.value,
+    'Age': form.age.value,
+    'Favorite Color': renderColor(form.favoriteColor.value),
+  }
+
+  const list = renderList(user)
+  const users = document.querySelector('#users')
+  users.appendChild(list)
+
+  form.reset()
+  form.userName.focus()
+}
+
 form.addEventListener('submit', handleSubmit)
